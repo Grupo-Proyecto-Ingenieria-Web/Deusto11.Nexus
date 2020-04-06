519cdd2af88b390b715203e2dd89b5481c4b98c8
Unknown <alejandro.jorge@opendeusto.es>
2020-04-06 17:41:46 +0200

    tarea1-Creacion del proyecto y la app

---
+++ deusto11_nexus/appdeusto11_nexus_components/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Appdeusto11NexusComponentsConfig(AppConfig):
+    name = 'appdeusto11_nexus_components'
---
+++ deusto11_nexus/deusto11_nexus_components/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Deusto11NexusComponentsConfig(AppConfig):
+    name = 'deusto11_nexus_components'
