2f6d7b1df7fc3347d1231a9834d0fe10b8732995
Asier <asier.marin@opendeusto.es>
2020-04-09 04:07:57 +0200

    app added to proyect, first urls created

--- deusto11_nexus/deusto11_nexus/settings.py
+++ deusto11_nexus/deusto11_nexus/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'deusto11_nexus_components' ,
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
