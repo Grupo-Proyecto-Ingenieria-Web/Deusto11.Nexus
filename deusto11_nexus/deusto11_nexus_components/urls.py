9361aa33b65a0f397f6ac5db4c777729a4f6373f
Asier <asier.marin@opendeusto.es>
2020-04-12 17:57:47 +0200

    static files and reuqest services added

--- deusto11_nexus/deusto11_nexus_components/urls.py
+++ deusto11_nexus/deusto11_nexus_components/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index, name='Index'),
+    path('empleado', views.Empleado, name='Empleado'),
+    path('tickets', views.Tickets, name='Tickets'),
+    path('vlog', views.Vlog, name='Vlog'),
 ]
--- deusto11_nexus/deusto11_nexus_components/views.py
+++ deusto11_nexus/deusto11_nexus_components/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import deusto11_nexus_services as nexus_services
+
+# _temaplateViews = nexus_services.TemplatesViews(request)
 
 # Create your views here.
-def index(request):
-
+def Index(request):
     return render(request, 'index.html')
+
+def Empleado(request):
+    return render(request, 'empleado.html')
+
+def Tickets(request):
+    return render(request, 'tickets.html')
+
+def Vlog(request):
+    return render(request, 'vlog.html')
