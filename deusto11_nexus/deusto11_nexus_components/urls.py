18e64e8c0748250b0695f26425e00d511d27df9e
Alexjorlo <alejandro.jorge@opendeusto.es>
2020-04-21 19:44:04 +0200

    Apaño en login

--- deusto11_nexus/deusto11_nexus_components/urls.py
+++ deusto11_nexus/deusto11_nexus_components/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import login, logout_then_login
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index_default_view'),
+    #para logearse
+    path('',login,{'template_name': 'index.html'}.name='login'),
+    #para salirte 
+    #path('logout/',logout_then_login,name='logout'),
     path('employerPortal/', views.EmployerPortalView.as_view(), name='employer_default_portal'),
     path('employerPortal/create/', views.EmployerRegistryView.as_view(), name='employer_create'),
     path('employerPortal/update/<int:pk>/', views.UpdateEmployerProfileView.as_view(), name='employer_update'),
