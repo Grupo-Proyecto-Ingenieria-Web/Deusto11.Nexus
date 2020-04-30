f60647b50c64a0b95405bd34792a5595379d72ea
Alexjorlo <alejandro.jorge@opendeusto.es>
2020-04-30 20:54:26 +0200

    cc

--- deusto11_nexus/deusto11_nexus_components/models.py
+++ deusto11_nexus/deusto11_nexus_components/models.py
@@ -13,7 +13,7 @@ class Machine(models.Model):
     # ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
 #Esto es para que se vea
     def __str__(self):
-        return f"id={self.id}, numero_serie={self.set_number}, modelo={self.model}, marca={self.brand}, tipo_equipo={self.machine_type}, fecha_adquisicion={self.get_date}, fecha_puesta_en_marcha={self.start_up_date}, proveedor_nombre={self.provider_name}, proveedor_telefono={self.provider_telefone}, planta={self.floor_on_premise}"
+        return f"modelo={self.model},  marca={self.brand}, tipo_equipo={self.machine_type},  planta={self.floor_on_premise}"
 #Aqui ponemos todos los atributos de ticket
 class Ticket(models.Model):
     reference_number = models.IntegerField(null=False, blank=False, default=101, unique=True)
@@ -29,7 +29,7 @@ class Ticket(models.Model):
     machine = models.ManyToManyField(Machine) 
 #Esto es para que se vea
     def __str__(self):
-        return f"id={self.id}, numero_referencia={self.reference_number}, titulo={self.title}, descripcion={self.description}, fecha_apertura={self.starting_date}, fecha_resolucion={self.resolution_date}, nivel_urgencia={self.urgency_level}, tipo={self.ticket_type}, estado={self.status}, comentario={self.comment}, machine{self.machine}"
+        return f" titulo={self.title}, descripcion={self.description},  nivel_urgencia={self.urgency_level}, tipo={self.ticket_type}, estado={self.status}, machine{self.machine}"
         
 #Aqui ponemos todos los atributos de empleado
 class Employee(models.Model):
@@ -43,7 +43,7 @@ class Employee(models.Model):
     ticket = models.ManyToManyField(Ticket)
 #Esto es para que se vea
     def __str__(self):
-        return f"id={self.id}, dni={self.dni}, nombre={self.name}, apellidos={self.surname}, email={self.email}, telefono={self.telefone_number}, nick={self.user_nick}, tocket= {self.ticket}, contrasena={self.password}"
+        return f" dni={self.dni}, nombre={self.name}, apellidos={self.surname}, email={self.email}, telefono={self.telefone_number}, nick={self.user_nick}, ticket= {self.ticket}, contrasena={self.password}"
 
 # Only model, not necessary migrate changes to database
 class EmployerLoginModel(models.Model):
